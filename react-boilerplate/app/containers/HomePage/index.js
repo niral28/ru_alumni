2566e421424565ba86777af1cf0c615ebbb11730
Augustus Chang <augustus.chang@rutgers.edu>
2018-01-15 21:40:16 -0500

    login page sorta done

--- react-boilerplate/app/containers/HomePage/index.js
+++ react-boilerplate/app/containers/HomePage/index.js
@@ -10,21 +10,25 @@
  */
 
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
+// import { FormattedMessage } from 'react-intl';
+import { Divider, Icon } from 'antd';
+// import messages from './messages';
+
 import LoginForm from '../../components/LoginForm';
 
 
 export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
-      <div style={{ display: 'flex', 'justify-content': 'center', 'align-items': 'center' }}>
-        <div>
-          <LoginForm />
-        </div>
+      <div style={{ flexDirection: 'column', display: 'flex', 'justify-content': 'center', 'align-items': 'center' }}>
         {/* <h1>
           <FormattedMessage {...messages.header} />
         </h1> */}
+        <Icon type="linkedin" style={{ fontSize: 100, color: '#08c' }} />
+        <Divider> OR </Divider>
+        <LoginForm />
+
+
       </div>
     );
   }
