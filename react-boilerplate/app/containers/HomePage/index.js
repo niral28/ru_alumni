06bf1fdef8154947c0bc98d0898f7ba278e08262
Augustus Chang <augustus.chang@rutgers.edu>
2018-01-15 21:48:11 -0500

    add some stylezzz

--- react-boilerplate/app/containers/HomePage/index.js
+++ react-boilerplate/app/containers/HomePage/index.js
@@ -20,13 +20,19 @@ import LoginForm from '../../components/LoginForm';
 export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
-      <div style={{ flexDirection: 'column', display: 'flex', 'justify-content': 'center', 'align-items': 'center' }}>
+      <div style={{ flexDirection: 'column', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
         {/* <h1>
           <FormattedMessage {...messages.header} />
         </h1> */}
-        <Icon type="linkedin" style={{ fontSize: 100, color: '#08c' }} />
-        <Divider> OR </Divider>
-        <LoginForm />
+        <div>
+          <Icon type="linkedin" style={{ fontSize: 100, color: '#08c' }} />
+        </div>
+        <div>
+          <Divider> OR </Divider>
+        </div>
+        <div>
+          <LoginForm />
+        </div>
 
 
       </div>
