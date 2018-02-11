aab45af84d3616194c3737bd4006a46446cf02bc
niral28 <niralshah928@gmail.com>
2018-02-10 22:26:58 -0500

    updating

--- application.py
+++ application.py
@@ -1,36 +1,8 @@
-from flask import Flask
+#!/usr/bin/env python
+from ru_alumni import application
 
-# print a nice greeting.
-def say_hello(username = "World"):
-    return '<p>Hello %s!</p>\n' % username
+DEBUG = True
 
-# some bits of text for the page.
-header_text = '''
-    <html>\n<head> <title>EB Flask Test</title> </head>\n<body>'''
-instructions = '''
-    <p>Hi guys! -parth</p>\n
-    <p> Hello - augustus </p>
-    <p> What's up - Niral </p>
-'''
 
-home_link = '<p><a href="/">Back</a></p>\n'
-footer_text = '</body>\n</html>'
-
-# EB looks for an 'application' callable by default.
-application = Flask(__name__)
-
-# add a rule for the index page.
-application.add_url_rule('/', 'index', (lambda: header_text +
-    say_hello() + instructions + footer_text))
-
-# add a rule when the page is accessed with a name appended to the site
-# URL.
-application.add_url_rule('/<username>', 'hello', (lambda username:
-    header_text + say_hello(username) + home_link + footer_text))
-
-# run the app.
-if __name__ == "__main__":
-    # Setting debug to True enables debug output. This line should be
-    # removed before deploying a production app.
-    application.debug = True
-    application.run()
+if __name__ == '__main__':
+    application.run(debug=DEBUG)
