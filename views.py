7d0a5a20ec2aedbfdef60ea008eddf11b44945ac
niral28 <niralshah928@gmail.com>
2018-02-10 22:09:31 -0500

    urls.py and cleaned up views.py

--- views.py
+++ views.py
@@ -3,7 +3,7 @@ from requests_oauthlib import OAuth2Session
 from requests_oauthlib.compliance_fixes import linkedin_compliance_fix
 import os 
 import keys
-
+import urls
 #for SSL
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
@@ -15,20 +15,9 @@ client_id = keys.LIN_CLIENT_KEY;
 client_secret = keys.LIN_SECRET_KEY;
 
  # OAuth endpoints given in the LinkedIn API documentation
-authorization_base_url = 'https://www.linkedin.com/uas/oauth2/authorization'
-token_url = 'https://www.linkedin.com/uas/oauth2/accessToken'
-reg_redirect = 'http://ru-alumni.36sn3vntw3.us-east-2.elasticbeanstalk.com/login'
-
-# print a nice greeting.
-def say_hello(username = "World"):
-    return '<p>Hello %s!</p>\n' % username
-
-# add a rule for the index page.
-application.add_url_rule('/', 'index', (lambda: header_text +
-    say_hello() + instructions + footer_text))
-
-application.add_url_rule('/<username>', 'hello', (lambda username:
-    header_text + say_hello(username) + home_link + footer_text))
+authorization_base_url = urls.OAUTH_BASE_URL
+token_url = urls.TOKEN_URL
+reg_redirect = urls.REDIRECT
 
 
 @application.route("/authorize")
@@ -49,12 +38,8 @@ def callback():
 @application.route("/profile", methods=["GET"])
 def profile():
     linkedin = OAuth2Session(client_id, redirect_uri=reg_redirect,token=session['oauth_token'])
-    r = jsonify(linkedin.get('https://api.linkedin.com/v1/people/~:(id,first-name,last-name,email-address,headline,num-connections,industry,picture-url,location)?format=json').json())
+    r = jsonify(linkedin.get(urls.USER_DATA).json())
     return r
-
-# print a nice greeting.
-def say_hello(username = "World"):
-    return '<p>Hello %s!</p>\n' % username
 
 # run the app.
 if __name__ == "__main__":
